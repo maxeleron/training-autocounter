daa88acee01ce751fe2feeae334f0e2ccf8c1bbb
maxeleron <maxeleron@gmail.com>
2021-02-27 19:22:06 +0200

    dev-001

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 "use strict"
+
+// UI elements
+const counter = document.getElementById('timeCounter');
+const secondInterval = document.getElementById('secondInterval');
+const secondIntervalFloat = document.getElementById('secondIntervalFloat');
+const startBtn = document.getElementById('startBtn');
+
+//var a = setInterval(func, 4 * 1000);
+let intervalExists = false;
+
+function gg(){
+    if(intervalExists) clearInterval(a);
+
+    let a = setInterval(() => {counter.innerHTML = +counter.innerHTML + 1;},
+    secondInterval.value * 1000 + secondIntervalFloat.nodeValue * 100);
+    intervalExists = true;
+}
